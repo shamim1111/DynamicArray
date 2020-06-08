aa576306e667f69309de357c1837d7ed6b0a3ab3
shamim1111 <shahazada8250@yahoo.co.uk>
2020-06-08 08:32:58 +0100

    Added the project.

---
+++ Dynamic_Array/DynamicArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Array
+{
+    class DynamicArray
+    {
+        /// <summary>
+        /// This method process the dynamic array based on the hacker rank algorithm.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
+        public List<int> ProcessDynamicArray(int n, List<List<int>> queries)
+        {
+            List<int> result = new List<int>();
+            List<int> seq;
+            List<List<int>> seqList = new List<List<int>>();
+            int lastAnswer = 0;
+            int seqListIndex = 0;
+            int size = 0;
+            for (int i = 0; i < n; i++)
+            {
+                seq = new List<int>();
+                seqList.Add(seq);
+            }
+            foreach (var subQuery in queries)
+            {   //when sub query value is 1
+                if (subQuery[0] == 1)
+                {
+                    seqListIndex = (subQuery[1] ^ lastAnswer) % n;
+
+                    seq = seqList[seqListIndex];
+                    seq.Add(subQuery[2]);
+                }
+                else
+                if (subQuery[0] == 2)// when sub query value is 2
+                {
+                    seqListIndex = (subQuery[1] ^ lastAnswer) % n;
+
+                    seq = seqList[seqListIndex];
+                    size = seq.Count;
+                    lastAnswer = seq[subQuery[2] % size];
+                    result.Add(lastAnswer);
+                   
+
+
+                }
+
+               
+
+            }
+
+            
+            return result;
+        }
+    }
+}
---
+++ Dynamic_Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///you can enter 2 5 respectively to check the outcome as Hacker Rank
+            Console.WriteLine("Please enter n and q value in the same line and with  the space then enter.");
+            string[] inputArray = Console.ReadLine().TrimEnd().Split(' ');
+            int nValue = Convert.ToInt32(inputArray[0]);
+            int qValue = Convert.ToInt32(inputArray[1]);
+            List<List<int>> queries = new List<List<int>>();
+            //Initialize sub queries based on the inputs from hacker rank
+            List<int> subQuery_01 = new List<int>() { 1, 0, 5 };
+            queries.Add(subQuery_01);
+            List<int> subQuery_02 = new List<int>() { 1, 1, 7 };
+            queries.Add(subQuery_02);
+            List<int> subQuery_03 = new List<int>() { 1, 0, 3 };
+            queries.Add(subQuery_03);
+            List<int> subQuery_04 = new List<int>() { 2, 1, 0 };
+            queries.Add(subQuery_04);
+            List<int> subQuery_05 = new List<int>() { 2, 1, 2 };
+            queries.Add(subQuery_05);
+           
+            DynamicArray dArray = new DynamicArray();
+            List<int> result = dArray.ProcessDynamicArray(nValue, queries);
+            Console.WriteLine(string.Join("\n", result));
+            Console.ReadKey();
+        }
+    }
+}
